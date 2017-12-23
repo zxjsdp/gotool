14ca30c35e4b23056ab4fecc6a839bd803d4c5a8
zxjsdp <zxjsdp@gmail.com>
2017-12-23 21:49:15 +0800

    Add strings

---
+++ strings.go
@@ -0,0 +1,63 @@
+package gotool
+
+import (
+	"strings"
+	"unicode"
+)
+
+// CleanElements clean whitespaces in strings.
+func CleanElements(strList []string) []string {
+	for index, each := range strList {
+		each = strings.TrimSpace(each)
+		strList[index] = each
+	}
+	return strList
+}
+
+// RemoveChar removes rune from string.
+func RemoveRune(source string, runeToBeRemoved rune) string {
+	return strings.Map(func(r rune) rune {
+		if r != runeToBeRemoved {
+			return r
+		}
+		return -1
+	}, source)
+}
+
+func RemoveBlankRunes(source string) string {
+	return strings.Map(func(r rune) rune {
+		if !unicode.IsSpace(r) {
+			return r
+		}
+		return -1
+	}, source)
+}
+
+// ReplaceRunes replaces blank chars in string with new rune.
+func ReplaceBlankRunes(source string, newRune rune) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return newRune
+		}
+		return r
+	}, source)
+}
+
+// ReplaceRunes replaces blank chars in string with new token.
+func ReplaceRunes(source string, oldRune, newRune rune) string {
+	return strings.Map(func(r rune) rune {
+		if r == oldRune {
+			return newRune
+		}
+		return r
+	}, source)
+}
+
+// CheckSubStringExistence checks whether substring exists in string.
+func CheckSubStringExistence(str, subString string) bool {
+	index := strings.Index(str, subString)
+	if index >= 0 {
+		return true
+	}
+	return false
+}
